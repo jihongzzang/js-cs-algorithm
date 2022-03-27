cb65491ba26013f39cf011662f6759329cdc784f
jihongzzang <jujihong2@gmail.com>
2022-03-27 13:04:53 +0900

    Add : 프로그래머스 신고결과 받기, 신고결과 추천

--- algorithm/1.js
+++ algorithm/1.js
@@ -1,3 +1,39 @@
-const hello = () => {
-  return console.log('hello,world');
+const solution = (id_list, report, k) => {
+  const answer = new Array(id_list.length);
+
+  answer.fill(0);
+
+  const report_list = {};
+
+  id_list.map(user_id => {
+    report_list[user_id] = [];
+  });
+
+  report.map(users => {
+    const [user_id, report_id] = users.split(' ');
+    if (!report_list[report_id].includes(user_id)) {
+      report_list[report_id].push(user_id);
+    }
+  });
+
+  for (key in report_list) {
+    if (report_list[key].length >= k) {
+      report_list[key].map(user => {
+        answer[id_list.indexOf(user)] += 1;
+      });
+    }
+  }
+  return answer;
 };
+
+const id_list = ['muzi', 'frodo', 'apeach', 'neo'];
+const report = [
+  'muzi frodo',
+  'apeach frodo',
+  'frodo neo',
+  'muzi neo',
+  'apeach muzi',
+];
+const k = 2;
+
+solution(id_list, report, k);
