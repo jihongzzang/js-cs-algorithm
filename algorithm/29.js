0b94814671a177172ffadc2a7f6aef233582084d
jihongzzang <jujihong2@gmail.com>
2023-09-12 22:41:46 +0900

    feat: 알고리즘(스택) 관련 업데이트

---
+++ algorithm/29.js
@@ -0,0 +1,77 @@
+//크레인 작동 시 인형이 집어지지 않는 경우는 없으나 만약 인형이 없는 곳에서 크레인을 작동 시키는 경우에는 아무런 일도 일어나지 않습니다. 또한 바구니는 모든 인형이 들어갈 수 있을 만큼 충분히 크다고 가정합니다. (그림에서는 화면표시 제약으로 5칸만으로 표현하였음) 게임 화면의 격자의 상태가 담긴 2차원 배열 board와 인형을 집기 위해 크레인을 작동시킨 위 치가 담긴 배열 moves가 매개변수로 주어질 때, 크레인을 모두 작동시킨 후 터트려져 사라진 인형의 개수를 return 하도록 solution 함수를 완성해주세요.
+
+let a = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+let b = [1, 5, 3, 5, 1, 2, 1, 4];
+
+function solution(board, moves) {
+  let answer = 0;
+  let stack = [];
+  moves.forEach(pos => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][pos - 1] !== 0) {
+        let tmp = board[i][pos - 1];
+        board[i][pos - 1] = 0;
+        if (tmp === stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        } else stack.push(tmp);
+        break;
+      }
+    }
+  });
+
+  return answer;
+}
+
+//후위연산식이 주어지면 연산한 결과를 출력하는 프로그램을 작성하세요.
+//만약 3*(5+2)-9 을 후위연산식으로 표현하면 352+*9- 로 표현되며 그 결과는 12입니다.
+
+function solution(s) {
+  let answer;
+  let stack = [];
+  for (let x of s) {
+    if (!isNaN(x)) stack.push(Number(x));
+    else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      if (x === '+') stack.push(lt + rt);
+      else if (x === '-') stack.push(lt - rt);
+      else if (x === '*') stack.push(lt * rt);
+      else if (x === '/') stack.push(lt / rt);
+    }
+  }
+  answer = stack[0];
+  return answer;
+}
+
+let str = '352+*9-';
+
+//1. 레이저는 여는 괄호와 닫는 괄호의 인접한 쌍 ‘( ) ’ 으로 표현된다. 또한, 모든 ‘( ) ’는 반 드시 레이저를 표현한다.
+
+//2. 쇠막대기의 왼쪽 끝은 여는 괄호 ‘ ( ’ 로, 오른쪽 끝은 닫힌 괄호 ‘) ’ 로 표현된다.
+
+//위 예의 괄호 표현은 그림 위에 주어져 있다. 쇠막대기는 레이저에 의해 몇 개의 조각으로 잘려지는데, 위 예에서 가장 위에 있는 두 개의 쇠막대기는 각각 3개와 2개의 조각으로 잘려지고, 이와 같은 방식으로 주어진 쇠막대기들은 총 17개의 조각으로 잘려진다.쇠막대기와 레이저의 배치를 나타내는 괄호 표현이 주어졌을 때, 잘려진 쇠막대기 조각의 총개수를 구하는 프로그램을 작성하시오.
+
+function solution(s) {
+  let answer = 0;
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') stack.push('(');
+    else {
+      stack.pop();
+      if (s[i - 1] === '(') answer += stack.length;
+      else answer++;
+      //stack.pop(); 이 위치에 하면 레이저까지 카운팅한다.
+    }
+  }
+  return answer;
+}
+
+let c = '()(((()())(())()))(())';
