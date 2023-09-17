623fe209616c35defdf3c57e3e41f466cc824908
jihongzzang <jujihong2@gmail.com>
2023-09-17 19:02:53 +0900

    feat: 삽입정렬

--- algorithm/32.js
+++ algorithm/32.js
@@ -12,7 +12,7 @@ function solution(arr) {
   return answer;
 }
 
-//버블정렬
+// 버블정렬
 function solution(arr) {
   let answer = arr;
   for (let i = 0; i < arr.length - 1; i++) {
@@ -21,6 +21,21 @@ function solution(arr) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
     }
+  }
+  return answer;
+}
+
+// 삽입정렬
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let tmp = arr[i],
+      j;
+    for (j = i - 1; j >= 0; j--) {
+      if (arr[j] > tmp) arr[j + 1] = arr[j];
+      else break;
+    }
+    arr[j + 1] = tmp;
   }
   return answer;
 }
