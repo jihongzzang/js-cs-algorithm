6b5905cef1827df2efc6e51cffdb9488a9bcccc8
jujihong <jujihong2@gmail.com>
2023-11-02 02:17:40 +0900

    feat: 프로그래머스 알고리즘

--- algorithm/53.js
+++ algorithm/53.js
@@ -49,3 +49,37 @@ function solution(str1, str2) {
 
   return answer;
 }
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12985 (예상 대진표)
+
+function solution(n, a, b) {
+  var answer = 0;
+
+  while (a !== b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    answer++;
+  }
+
+  return answer;
+}
+
+function solution(n, a, b) {
+  var answer = 0;
+
+  while (n > 0) {
+    answer++;
+
+    if (
+      (a + 1 === b && a % 2 !== 0 && b % 2 === 0) ||
+      (a === b + 1 && a % 2 === 0 && b % 2 !== 0)
+    ) {
+      break;
+    }
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    n / 2;
+  }
+
+  return answer;
+}
