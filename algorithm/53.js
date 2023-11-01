63a3bc2fd4e249ad5e7c284099adbb006c8722ec
jujihong <jujihong2@gmail.com>
2023-11-02 02:00:10 +0900

    feat: 프로그래머스 알고리즘

---
+++ algorithm/53.js
@@ -0,0 +1,51 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/17677 (뉴스 클러스터링)
+
+const regex = /^[a-zA-Z]*$/;
+
+function solution(str1, str2) {
+  var answer = 0;
+  let str1Arr = [];
+  let str2Arr = [];
+
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  for (let i = 0; i < str1.length - 1; i++) {
+    const targetText = str1[i] + str1[i + 1];
+    if (regex.test(targetText)) {
+      str1Arr.push(targetText);
+    }
+  }
+
+  for (let i = 0; i < str2.length - 1; i++) {
+    const targetText = str2[i] + str2[i + 1];
+    if (regex.test(targetText)) {
+      str2Arr.push(targetText);
+    }
+  }
+
+  if (str1Arr.length === 0 && str2Arr.length === 0) return 65536;
+  str1Arr.sort();
+  str2Arr.sort();
+
+  if (JSON.stringify(str1Arr) === JSON.stringify(str2Arr)) return 65536;
+
+  const tempStr2Arr = [...str2Arr];
+
+  const intersectionArr = str1Arr.reduce((acc, curr) => {
+    if (tempStr2Arr.includes(curr)) {
+      const index = tempStr2Arr.indexOf(curr);
+      tempStr2Arr.splice(index, 1);
+      return [...acc, curr];
+    } else {
+      return acc;
+    }
+  }, []);
+
+  const unionArrlength =
+    str1Arr.length + str2Arr.length - intersectionArr.length;
+
+  answer = Math.floor((intersectionArr.length / unionArrlength) * 65536);
+
+  return answer;
+}
