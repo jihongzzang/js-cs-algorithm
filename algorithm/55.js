271b03203868c7af9c628dd02650dc132623a8d0
jujihong <jujihong2@gmail.com>
2023-11-04 23:48:29 +0900

    feat: 프로그래머스 알고리즘

--- algorithm/55.js
+++ algorithm/55.js
@@ -43,3 +43,29 @@ function solution(arr) {
 
   return set.size;
 }
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42885 (구명보트)
+
+function solution(people, limit) {
+  var answer = 0;
+
+  let left = 0;
+  let right = people.length - 1;
+
+  if (people.length === 1) return 1;
+
+  people.sort((a, b) => b - a);
+
+  while (left <= right) {
+    answer++;
+
+    if (people[left] + people[right] <= limit) {
+      left++;
+      right--;
+    } else {
+      left++;
+    }
+  }
+
+  return answer;
+}
