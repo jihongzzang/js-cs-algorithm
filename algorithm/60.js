be1d21442a4e5a24d2d4529978345e248b82c25e
jujihong <jujihong2@gmail.com>
2023-11-14 01:27:31 +0900

    feat: 프로그래머스 오픈채팅방

--- algorithm/60.js
+++ algorithm/60.js
@@ -24,7 +24,6 @@ function solution(order) {
 }
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/49994 (방문길이)
-
 function solution(dirs) {
   let move = { L: [-1, 0], R: [1, 0], U: [0, 1], D: [0, -1] };
   let now = [0, 0];
@@ -43,4 +42,27 @@ function solution(dirs) {
   }
 
   return route.size / 2;
+}
+
+//school.programmers.co.kr/learn/courses/30/lessons/42888 (오픈채팅방)
+https: function solution(record) {
+  let answer = [];
+  const map = new Map();
+
+  for (let i = 0; i < record.length; ++i) {
+    const [state, uid, name] = record[i].split(' ');
+
+    if (state == 'Leave') {
+      answer.push([uid, '님이 나갔습니다.']);
+      continue;
+    }
+
+    if (state == 'Enter') {
+      answer.push([uid, '님이 들어왔습니다.']);
+    }
+
+    map.set(uid, name);
+  }
+
+  return answer.map(ele => map.get(ele[0]) + ele[1]);
 }
